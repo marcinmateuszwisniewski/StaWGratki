d3098d1aed46e58dfe065721f9ed89794b525d0f
Marcin W <marcinmateuszwisniewski@gmail.com>
2020-06-09 17:40:34 +0200

    refactoring method to *ackchually* throw an exception

--- src/main/java/UserInput/ShipSetupCommandLine.java
+++ src/main/java/UserInput/ShipSetupCommandLine.java
@@ -2,38 +2,49 @@ package UserInput;
 
 import Ships.Coordinate;
 
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class ShipSetupCommandLine implements CommandLineInterface{
+public class ShipSetupCommandLine implements CommandLineInterface {
     private final Scanner scanner = new Scanner(System.in);
     private final CoordinateParser parser;
 
-    public ShipSetupCommandLine (CoordinateParser parser){
+    public ShipSetupCommandLine(CoordinateParser parser) {
         this.parser = parser;
     }
 
-    public ShipSetupResponse read(){
+    public ShipSetupResponse read() throws IllegalArgumentException{
 
-        System.out.println("give date in format: nameOfShip c1 c2 3");
+        System.out.println("give ship info in format: nameOfShip c1 c2 3");
         System.out.println("nameOfShip - duh");
         System.out.println("c1 c2 - first two coordinates (their order matters!), must be side-by-side");
         System.out.println("3 - overall length of ship");
         String result = scanner.nextLine();
         StringTokenizer stringTokenizer = new StringTokenizer(result);
-        String shipName = stringTokenizer.nextToken();
-        String firstCoord = stringTokenizer.nextToken();
-        String secondCoord = stringTokenizer.nextToken();
+        String shipName;
+        String firstCoord;
+        String secondCoord;
+        int length;
+
+        try {
+            shipName = stringTokenizer.nextToken();
+            firstCoord = stringTokenizer.nextToken();
+            secondCoord = stringTokenizer.nextToken();
+            length = Integer.parseInt(stringTokenizer.nextToken());
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("your input lacks something", e);
+
+        }
 
         Coordinate firstC;
         Coordinate secondC;
-        int length;
 
         firstC = parser.parseCoordinates(firstCoord);
         secondC = parser.parseCoordinates(secondCoord);
-        length = Integer.parseInt(stringTokenizer.nextToken());
 
-        return new ShipSetupResponse(shipName,firstC,secondC,length);
+
+        return new ShipSetupResponse(shipName, firstC, secondC, length);
 
     }
 
