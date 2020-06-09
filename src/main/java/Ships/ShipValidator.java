9e0a1bfbdab14bca144a08cf19f97e0226defa12
Marcin W <marcinmateuszwisniewski@gmail.com>
2020-06-09 11:47:51 +0200

    Ship Validator for setup

---
+++ src/main/java/Ships/ShipValidator.java
@@ -0,0 +1,20 @@
+package Ships;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ShipValidator {
+    public static boolean validate (Ship shipToPut, Set<Ship> shipSet){
+
+        Set<Coordinate> allPosistions = shipSet.stream().map(ship -> ship.position).flatMap(Collection::stream).collect(Collectors.toSet());
+        for(Coordinate c : shipToPut.getPosition()){
+            if (allPosistions.contains(c)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
