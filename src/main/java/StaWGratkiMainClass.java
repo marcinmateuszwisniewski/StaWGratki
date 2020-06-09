0d2f7ced5960b5ac594fc3fba902201159bed5ec
Marcin W <marcinmateuszwisniewski@gmail.com>
2020-06-09 11:56:49 +0200

    main updated

--- src/main/java/StaWGratkiMainClass.java
+++ src/main/java/StaWGratkiMainClass.java
@@ -1,6 +1,11 @@
 import Boards.BoardDisplay;
 import GameFlow.GameStart;
+import GameFlow.ShipSetup;
+import GameFlow.ShipSetupInterface;
 import GameState.GameStateService;
+import Ships.Dockyard;
+import UserInput.CoordinateParser;
+import UserInput.ShipSetupCommandLine;
 import UserInput.StartGameCommandLine;
 
 import java.util.Scanner;
@@ -16,7 +21,9 @@ public class StaWGratkiMainClass {
       GameStart gameStart = new GameStart(new StartGameCommandLine(scanner),gs);
       gameStart.init();
       BoardDisplay bd = new BoardDisplay(gs);
-      bd.printBoard();
+      bd.printBothBoards();
+      ShipSetupInterface setup = new ShipSetup(new ShipSetupCommandLine(new CoordinateParser()),gs,new Dockyard(),bd);
+      setup.setup();
 
         
     }
