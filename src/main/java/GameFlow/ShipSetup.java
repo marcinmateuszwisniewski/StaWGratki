5c271514eac335bd05429da610b000c98f136f5c
Marcin W <marcinmateuszwisniewski@gmail.com>
2020-06-09 11:56:32 +0200

    ship setup service

--- src/main/java/GameFlow/ShipSetup.java
+++ src/main/java/GameFlow/ShipSetup.java
@@ -1,18 +1,73 @@
 package GameFlow;
 
+import Boards.Board;
+import Boards.BoardDisplay;
+import Boards.BoardValidator;
+import GameState.GameState;
 import GameState.GameStateService;
+import Ships.Ship;
 import Ships.ShipFactory;
+import Ships.ShipValidator;
+import UserInput.CommandLineInterface;
 import UserInput.ShipSetupCommandLine;
+import UserInput.ShipSetupResponse;
+import org.apache.commons.collections4.Bag;
+import org.apache.commons.collections4.bag.HashBag;
 
-public class ShipSetup {
+import java.util.HashSet;
 
-    private ShipSetupCommandLine commandLine;
+public class ShipSetup implements ShipSetupInterface{
+
+    private CommandLineInterface commandLine;
     private GameStateService gameStateService;
     private ShipFactory shipFactory;
-    public ShipSetup(ShipSetupCommandLine commandLine, GameStateService gameStateService, ShipFactory shipFactory){
+    private BoardDisplay boardDisplay;
+    public ShipSetup(CommandLineInterface commandLine, GameStateService gameStateService, ShipFactory shipFactory, BoardDisplay bd){
         this.commandLine = commandLine;
         this.gameStateService = gameStateService;
         this.shipFactory = shipFactory;
+        this.boardDisplay = bd;
     }
 
+
+    @Override
+    public void setup() {
+
+        HashSet<Ship> ships = new HashSet<>();
+        GameState gameState = gameStateService.getState();
+
+        Bag<Integer> numberOfShips = new HashBag<>();
+        numberOfShips.add(5);
+        numberOfShips.add(4);
+        numberOfShips.add(3);
+        numberOfShips.add(2,2);
+
+        while(!numberOfShips.isEmpty()){
+            ShipSetupResponse response;
+            response = (ShipSetupResponse) commandLine.read();
+
+            Ship newShip = null;
+            while (newShip == null){
+                try{
+                    newShip = shipFactory.launch(response.getShipName(), response.getFirstCoord(), response.getSecondCoord(), response.getLength());
+                    System.out.println("ship:"+response.getShipName()+" created");
+
+                    if(ShipValidator.validate(newShip,ships)){
+                        ships.add(newShip);
+                        numberOfShips.remove(newShip.getOriginalLength());
+                        System.out.println("Ship added: "+newShip.getName());
+                        boardDisplay.printShipsSetup(ships);
+
+                    }
+
+
+                } catch (IllegalArgumentException e){
+                    System.out.println("Bad params"+ e.getMessage()+ " Provide correct input");
+                }
+            }
+
+
+        }
+
+    }
 }
